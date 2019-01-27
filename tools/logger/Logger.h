a858a7a86b784931ff2cfda7897109b0bc620812
ghoel <ghoel@dbpotato.net>
2019-01-27 21:47:18 +0100

    Add support for multiple loggers

--- tools/logger/Logger.h
+++ tools/logger/Logger.h
@@ -24,25 +24,30 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #pragma once
 
 #ifndef DISABLE_SPD_LOGGER
+
 #include <spdlog/spdlog.h>
 
 class func_sink : public spdlog::sinks::sink {
 public:
   func_sink():
-      _log_func(nullptr) {
+      _log_func(nullptr),
+      _log_func_arg(nullptr) {
   }
   virtual ~func_sink(){;}
-  static std::shared_ptr<func_sink> instance() {
-    static std::shared_ptr<func_sink> instance = std::make_shared<func_sink>();
-    return instance;
+  static std::shared_ptr<func_sink> create() {
+    std::shared_ptr<func_sink> sink = std::make_shared<func_sink>();
+    return sink;
   }
-  void SetLogFunc(void(*func)(spdlog::level::level_enum, const std::string&)){
+  void SetLogFunc(void(*func)(spdlog::level::level_enum, const std::string&, void*), void* arg){
     _log_func = func;
+    _log_func_arg = arg;
   }
   void log(const spdlog::details::log_msg& msg) override {
     std::lock_guard<std::mutex> lock(_mutex);
-    if(_log_func)
-      _log_func(msg.level, std::string(msg.formatted.data(), msg.formatted.size()));
+    if(_log_func) {
+      if(msg.formatted.size())
+        _log_func(msg.level, std::string(msg.formatted.data(), msg.formatted.size() - 1), _log_func_arg);
+    }
     else{
       fwrite(msg.formatted.data(), sizeof(char), msg.formatted.size(), stdout);
       flush();
@@ -54,31 +59,52 @@ protected:
   }
 
   std::mutex _mutex;
-  void(*_log_func)(spdlog::level::level_enum lv, const std::string&);
+  void(*_log_func)(spdlog::level::level_enum, const std::string&, void*);
+  void* _log_func_arg;
 };
+
+
 
 class Logger {
 public:
-  Logger() {
-    _logger = spdlog::details::registry::instance().create("console", func_sink::instance());
-    _logger->set_pattern("[%H:%M:%S:%e] [%t] [%l] %v");
-#ifdef ENABLE_DEBUG_LOGGER
-    _logger->set_level(spdlog::level::debug);
-#endif
-  }
   static Logger& Instance() {
     static Logger instance;
     return instance;
   }
-  void SetLogFunc(void(*func)(spdlog::level::level_enum, const std::string&)) {
-    func_sink::instance()->SetLogFunc(func);
+  void SetLogFunc(void(*func)(spdlog::level::level_enum, const std::string&, void*), int id_looger, void* log_func_arg) {
+    auto sink = std::static_pointer_cast<func_sink>(GetLogger(id_looger)->sinks()[0]);
+    sink->SetLogFunc(func, log_func_arg);
   }
+  std::shared_ptr<spdlog::logger> GetLogger(int id) {
+    if(!id)
+      return _default_logger;
 
-  std::shared_ptr<spdlog::logger> _logger;
+    auto logger = spdlog::details::registry::instance().get(std::to_string(id));
+    if(logger)
+      return logger;
+
+    logger = spdlog::details::registry::instance().create(std::to_string(id), func_sink::create());
+    return logger;
+  }
+protected:
+  Logger() {
+    _default_logger = spdlog::details::registry::instance().create("0", func_sink::create());
+    _default_logger->set_pattern("[%H:%M:%S:%e] [%t] [%l] %v");
+#ifdef ENABLE_DEBUG_LOGGER
+    _default_logger->set_level(spdlog::level::debug);
+#endif
+  }
+  std::shared_ptr<spdlog::logger> _default_logger;
 };
 
-static std::shared_ptr<spdlog::logger> log(){
-  return Logger::Instance()._logger;
+static std::shared_ptr<spdlog::logger> log(int id = 0) {
+  return Logger::Instance().GetLogger(id);
+}
+
+static void set_log_func(void(*func)(spdlog::level::level_enum, const std::string&, void*),
+                         int id_looger = 0,
+                         void* log_func_arg = nullptr) {
+  Logger::Instance().SetLogFunc(func, id_looger, log_func_arg);
 }
 
 #else
@@ -101,7 +127,7 @@ public:
 };
 
 
-static NoSPDLogger* log(){
+static NoSPDLogger* log(int id = 0){
   return &NoSPDLogger::Instance();
 }
 
