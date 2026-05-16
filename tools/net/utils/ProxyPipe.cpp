b759ebebf0af973734494b8c8cb69c13211b1b5d
adam <adam@dbpotato.net>
2026-05-16 23:30:53 +0200

    Fix compile error in ProxyPipe.cpp

--- tools/net/utils/ProxyPipe.cpp
+++ tools/net/utils/ProxyPipe.cpp
@@ -23,7 +23,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "ProxyPipe.h"
 #include "Logger.h"
-#include "ThreadLoop.h
+#include "ThreadLoop.h"
 
 
 const uint32_t DEFAULT_STOP_READ = 32;
